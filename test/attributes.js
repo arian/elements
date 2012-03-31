86026212d7c7864b9d156beb63a401dc7a0e62d8
Arian <arian@aryweb.nl>
2012-04-01 00:27:52 +0200

    Hack into mocha to make IE work. The end event did not emit in IE. Also use onreadystatechange for.

--- test/attributes.js
+++ test/attributes.js
@@ -10,5 +10,11 @@ describe('attributes', function(){
 		expect($(div).id()).to.equal('test')
 	})
 
+	it('should get the attrubte of a form when the form has an input with as ID the attribute name', function(){
+		var div = document.createElement('div')
+		div.innerHTML = '<form action="s"><input id="action"></form>'
+		expect($(div.firstChild).getAttribute('action')).to.equal('s')
+	})
+
 })
 
